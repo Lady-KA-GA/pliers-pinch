0efaea3bc1b5f84658e22c76d7f73aa12ed390cd
かがわ　ゆうと <yuto@kagawayuuto-no-MacBook-Pro.local>
2017-05-10 01:55:46 +0900

    ステージ選択画面でボタン移動時のSEを追加

---
+++ pinch animals/Assets/Script/BottomSound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomSound : MonoBehaviour 
+{
+	private AudioSource sound;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			sound.PlayOneShot(sound.clip);
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			sound.PlayOneShot(sound.clip);
+		}
+	}
+}
