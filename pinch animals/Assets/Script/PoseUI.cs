33512e3116fbcaa8d703452a3b9a719d04919907
かがわ　ゆうと <yuto@kagawayuuto-no-MacBook-Pro.local>
2017-05-12 23:41:03 +0900

    ポーズ画面にボタンを表示、非表示を管理するスクリプトを作成

---
+++ pinch animals/Assets/Script/PoseUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//ポーズUIのオンオフを管理するクラス
+public class PoseUI : MonoBehaviour 
+{
+	public GameObject poseui;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//ポーズ画面ではないなら
+		if (Pose.i == false)
+		{
+			//ポーズ画面でない状態ならばボタン（UIグループ）を無効化
+			if (poseui.gameObject.activeSelf)
+			{
+				poseui.gameObject.SetActive(false);
+			}
+		}
+		//ポーズ画面であれば
+		else 
+		{
+			//ポーズ画面であればボタン（UIグループ）を有効化
+			if (poseui.gameObject.activeSelf == false)
+			{
+				poseui.gameObject.SetActive(true);
+			}
+		}
+	}
+}
