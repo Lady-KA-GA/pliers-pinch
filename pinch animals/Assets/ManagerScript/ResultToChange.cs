6fa6d82747e8a0c668a06c709550939262ab38eb
かがわ　ゆうと <yuto@kagawayuuto-no-MacBook-Pro.local>
2017-05-14 14:45:57 +0900

    ゲームオーバーからステージ選択へ遷移はするがボタンを非表示にしてゲームオーバーになった瞬間何からのエラーが出ているので完全ではない 現在原因追求中です

---
+++ pinch animals/Assets/ManagerScript/ResultToChange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultToChange : MonoBehaviour 
+{
+
+	public void PoseDeactive() 
+	{
+		Time.timeScale = 1;
+		gameover.overCheck = false;
+	}
+}
---
+++ pinch animals/Assets/Script/GameOverUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+//ポーズUIのオンオフを管理するクラス
+public class GameOverUI : MonoBehaviour 
+{
+	public GameObject gameoverui;
+
+	public Button Stage;
+
+	void Start()
+	{
+		// ボタンコンポーネントの取得
+		//Stage = GameObject.Find("/Canvas/PoseUI/Button").GetComponent<Button>();
+
+		/*if (Stage != null)
+		{
+			// 最初に選択状態にしたいボタンの設定
+			Stage.Select();
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//ゲームオーバー画面ではないなら
+		if (gameover.overCheck == false)
+		{
+			//ゲームオーバー画面でない状態ならばボタン（UIグループ）を無効化
+			if (gameoverui.gameObject.activeSelf)
+			{
+				gameoverui.gameObject.SetActive(false);
+				//Debug.Log("s");
+			}
+		}
+		//ゲームオーバー画面画面であれば
+		else 
+		{
+			//ゲームオーバー画面であればボタン（UIグループ）を有効化
+			if (gameoverui.gameObject.activeSelf == false)
+			{
+				gameoverui.gameObject.SetActive(true);
+			}
+		}
+	}
+}
